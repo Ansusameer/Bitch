69e03bdb736690b5e56c637386df5467397d80ee
Ansusameer <ansusameer@outlook.com>
2024-09-16 23:09:22 +0530

    Update info.py

--- info.py
+++ info.py
@@ -25,9 +25,9 @@ DWLD = environ.get("DWLD", 'AvaFlixx/9')
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
 
-PICS = (environ.get('PICS', 'https://te.legra.ph/file/d6a23f16e002e86381656.jpg https://te.legra.ph/file/d6a23f16e002e86381656.jpg')).split()
+PICS = (environ.get('PICS', 'https://envs.sh/wvy.jpg https://envs.sh/wvX.jpg https://envs.sh/wvM.jpg')).split()
 NOR_IMG = environ.get("NOR_IMG", "https://te.legra.ph/file/e709716b0b99ed944a533.jpg")
-MELCOW_PIC = environ.get("MELCOW_PIC", "https://graph.org/file/75d4fcc66598ea1ef4941.jpg")
+MELCOW_PIC = environ.get("MELCOW_PIC", "https://envs.sh/wv-.jpg")
 SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/2d68d2c0fbf9385d48d70.jpg")
 
 # Admins, Channels & Users
